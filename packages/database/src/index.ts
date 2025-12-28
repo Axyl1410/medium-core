9cb11ae03a97d8529d1c0ef60f8cf3b4636c2b39
Alex <truonggiang190689@gmail.com>
2025-12-29 02:27:26 +0700

    Refactor database connection to use neon database client, updating drizzle initialization for improved compatibility.

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,14 +1,15 @@
 import { assertValue } from "@workspace/utils";
 import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./db/schema";
 
 const databaseUrl = assertValue(
   process.env.DATABASE_URL,
   "DATABASE_URL is not set"
 );
 
-export const db = drizzle(databaseUrl, { schema });
+const sql = neon(databaseUrl);
+export const db = drizzle({ client: sql });
 
 export * from "drizzle-orm";
 export * from "./db/schema";
